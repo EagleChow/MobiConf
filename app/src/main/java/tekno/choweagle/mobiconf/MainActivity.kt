1f1b10ae513da209c5c93e48b772e4529db0a157
Chow Eagle <choweagle183@outlook.com>
2025-05-18 15:36:03 +0800

    创建此项目

---
+++ app/src/main/java/tekno/choweagle/mobiconf/MainActivity.kt
@@ -0,0 +1,13 @@
+package tekno.choweagle.mobiconf
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.core.view.WindowCompat
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+    }
+}
